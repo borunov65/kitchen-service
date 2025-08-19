8670a5c499ca55fef5256086bbbb96169c23a930
Ihor Borunov <borunov65@gmail.com>
2025-08-20 01:09:03 +0200

    Update build.sh to install requirements.txt

--- build.sh
+++ build.sh
@@ -8,16 +8,9 @@ if [ -f .env ]; then
   export $(grep -v '^#' .env | xargs)
 fi
 
-# Встановлюємо Poetry, якщо він ще не встановлений
-if ! command -v poetry &> /dev/null
-then
-    echo "Poetry not found, installing..."
-    curl -sSL https://install.python-poetry.org | python3 -
-    export PATH="$HOME/.local/bin:$PATH"
-fi
-
-# Встановлюємо залежності
-poetry install --no-root
+# Оновлюємо pip і встановлюємо залежності
+pip install --upgrade pip
+pip install -r requirements.txt
 
 # Міграції
 python3 manage.py migrate
